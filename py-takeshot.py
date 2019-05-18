c29b5bf2118fb5ed3ea6750e9f423e54bcb993aa
Sandeep Pushp <33807748+Sandeeppushp@users.noreply.github.com>
2019-05-19 02:27:17 +0530

    Add files via upload

---
+++ py-takeshot.py
@@ -0,0 +1,48 @@
+import pyscreenshot as ImageGrab
+import datetime
+
+import pyHook
+import pythoncom
+from pynput.keyboard import Key, Listener
+from pynput import keyboard
+
+
+def on_keypress(key):
+    d_date = datetime.datetime.now()
+    reg_format_date = d_date.strftime("%d-%m-%Y%I:%M:%S %p")
+    reg_format_date=reg_format_date.replace(':','-')
+    print(key)
+
+    im = ImageGrab.grab()
+    im.save(reg_format_date+'.jpg')
+
+
+key_listener = keyboard.Listener(on_release=on_keypress)
+key_listener.start()
+
+def onclick(event):
+	d_date = datetime.datetime.now()
+	reg_format_date = d_date.strftime("%d-%m-%Y%I:%M:%S %p")
+	reg_format_date=reg_format_date.replace(':','-')
+
+	print (event.Position)
+
+	im = ImageGrab.grab()
+	im.save(reg_format_date+'.jpg')
+	return True
+
+
+def main():
+	hm = pyHook.HookManager()
+	hm.SubscribeMouseAllButtonsDown(onclick)
+	hm.HookMouse()
+	pythoncom.PumpMessages()
+	hm.UnhookMouse()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
